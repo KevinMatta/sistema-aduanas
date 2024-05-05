71d4cf4e0ac6cc4d2a83a1ec3d7e46f5318800c1
Victor Espinoza <vespinozabenitez@gmail.com>
2024-05-05 16:51:45 -0600

    feat(app) Intentando dibujar el menu por esquema
    feat(app) Intentando dibujar el menu por esquema

--- app/src/app/components/form-declaracion-valor/form-declaracion-valor.component.ts
+++ app/src/app/components/form-declaracion-valor/form-declaracion-valor.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-// import { MatDialog } from '@angular/material/dialog';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form-declaracion-valor',
@@ -13,13 +13,26 @@ export class FormDeclaracionValorComponent implements OnInit {
   ngOnInit() {
   }
 
-  constructor() {}
+  closeResult = '';
 
-  openDialog() {
-    // this.dialog.open(DialogElementsExampleDialog);
-    
-    this.display = "true";
-    console.log(this.display);
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   input24: string = "- SELECCIONE -";
--- app/src/app/components/sidebar/sidebar.component.ts
+++ app/src/app/components/sidebar/sidebar.component.ts
@@ -8,27 +8,26 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
-    { path: '/index-usuarios', title: 'Usuarios',  icon:'text_caps-small', class: '', },
-    { path: '/form-usuarios', title: 'Usuarios',  icon:'text_caps-small', class: '', },
-    { path: '/index-roles', title: 'Roles',  icon:'text_caps-small', class: '', },
-    { path: '/index-aduanas', title: 'Aduanas',  icon:'text_caps-small', class: '', },
-    { path: '/index-empleados', title: 'Empleados',  icon:'text_caps-small', class: '', },
-    { path: '/index-paises', title: 'Paises',  icon:'text_caps-small', class: '', },
-    { path: '/index-estados', title: 'Estados',  icon:'text_caps-small', class: '', },
-    { path: '/index-ciudades', title: 'Ciudades',  icon:'text_caps-small', class: '', },
-    { path: '/index-empresas', title: 'Empresas',  icon:'text_caps-small', class: '', },
-    { path: '/index-estados-civiles', title: 'Estados Civiles',  icon:'text_caps-small', class: '', },
+    { path: '/index-usuarios', title: 'Acce.Usuarios',  icon:'text_caps-small', class: '', },
+    { path: '/form-usuarios', title: 'Acce.Usuarios',  icon:'text_caps-small', class: '', },
+    { path: '/index-roles', title: 'Acce.Roles',  icon:'text_caps-small', class: '', },
+    { path: '/index-aduanas', title: 'Adua.Aduanas',  icon:'text_caps-small', class: '', },
+    { path: '/index-empleados', title: 'Gral.Empleados',  icon:'text_caps-small', class: '', },
+    { path: '/index-paises', title: 'Gral.Paises',  icon:'text_caps-small', class: '', },
+    { path: '/index-estados', title: 'Gral.Estados',  icon:'text_caps-small', class: '', },
+    { path: '/index-ciudades', title: 'Gral.Ciudades',  icon:'text_caps-small', class: '', },
+    { path: '/index-empresas', title: 'Adua.Empresas',  icon:'text_caps-small', class: '', },
+    { path: '/index-estados-civiles', title: 'Gral.Estados Civiles',  icon:'text_caps-small', class: '', },
     { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
-    { path: '/form-persona-natural', title: 'Persona Natural',  icon:'users_single-02', class: '' },
-    { path: '/form-comerciante-individual', title: 'Comerciante individual',  icon:'users_single-02', class: '' },
-    { path: '/form-persona-juridica', title: 'Persona Juridica',  icon:'shopping_shop', class: '' },
-    { path: '/form-declaracion-valor', title: 'Declaración de valor',  icon:'files_single-copy-04', class: '' },
-    { path: '/boletin', title: 'Boletín de pago',  icon:'files_paper', class: '' },
+    { path: '/form-persona-natural', title: 'Adua.Persona Natural',  icon:'users_single-02', class: '' },
+    { path: '/form-comerciante-individual', title: 'Adua.Comerciante individual',  icon:'users_single-02', class: '' },
+    { path: '/form-persona-juridica', title: 'Adua.Persona Juridica',  icon:'shopping_shop', class: '' },
+    { path: '/form-declaracion-valor', title: 'Adua.Declaración de valor',  icon:'files_single-copy-04', class: '' },
+    { path: '/boletin', title: 'Adua.Boletín de pago',  icon:'files_paper', class: '' },
     { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
     // { path: '/login', title: 'Iniciar Sesión',  icon:'objects_spaceship', class: 'active active-pro' }
-
 ];
 
 @Component({
@@ -37,6 +36,32 @@ export const ROUTES: RouteInfo[] = [
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+
+  public isCollapsed: { [key: string]: boolean } = {
+    Acce: true,
+    Gral: true,
+    Adua: true,
+  };
+  // public isAccesoCollapsed = true;
+  // public isGeneralCollapsed = true;
+  // public isAduanaCollapsed = true;
+
+  toggleCollapse(schema: string) {
+    this.isCollapsed[schema] = !this.isCollapsed[schema];
+  }
+
+  belongsToSchema(menuItem: RouteInfo, schema: string): boolean {
+    return menuItem.title.includes(schema);
+  }
+
+  belongsToAnySchema(menuItem: RouteInfo): boolean {
+    return !['Acce', 'Gral', 'Adua'].some((schema) =>{ 
+        console.log(this.belongsToSchema(menuItem, schema), menuItem, schema);
+        return this.belongsToSchema(menuItem, schema)
+      }
+    );
+  }  
+
   menuItems: any[];
 
   constructor() { }
